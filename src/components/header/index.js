006d79abd551a2c47f2514fc50a42ed63597bf5a
Andrew Carlson <andrew@bubinga.co>
2018-03-02 08:48:41 -0600

    Completed header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './header.scss';
 
 class Header extends Component {
     render() {
         return(
             <header className="aic-header">
-                <Link to="/" className="aic-logo">aic</Link>
+                <Link to="/" className="aic-logo aic-pull-forward">
+                    <span>aic</span>
+                </Link>
             </header>
         )
     }
