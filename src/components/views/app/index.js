a06df2c0848d3f705ed6239c334159293142333f
Andrew Carlson <andrew@bubinga.co>
2018-03-02 08:15:28 -0600

    Updates references from CSS to SCSS

--- src/components/views/app/index.js
+++ src/components/views/app/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../../header';
 import Home from '../home';
-import './app.css';
+import './app.scss';
 
 class App extends Component {
   render() {
